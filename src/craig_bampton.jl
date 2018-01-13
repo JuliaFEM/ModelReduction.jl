6c1f9cb7daf9da15fc2f8cea6b6788ca39a042a4
Marja Rapo <mra.rapo@gmail.com>
2018-01-13 17:10:07 +0200

    Update craig_bampton.jl

--- src/craig_bampton.jl
+++ src/craig_bampton.jl
@@ -12,12 +12,13 @@ r = retained DOF:s, l = internal DOF:s, n = the number of modes to keep.
 function craig_bampton(K, M, r, l, n)
     Krr = K[r,r]; Krl = K[r,l]; Klr = K[l,r]; Kll = K[l,l]
     Mrr = M[r,r]; Mrl = M[r,l]; Mlr = M[l,r]; Mll = M[l,l]
-    if issparse(K) && issparse(M)
-        w2, X1 = eigs(Kll, Mll)
-    else
-        w2 = eigvals(Kll,Mll)
-        X1 = eigvecs(Kll,Mll)
-    end
+    #if issparse(K) && issparse(M)
+    #    w2, X1 = eigs(Kll, Mll)
+    #else
+    #    w2 = eigvals(Kll,Mll)
+    #    X1 = eigvecs(Kll,Mll)
+    #end
+    w2 = eigvals(Kll,Mll); X1 = eigvecs(Kll,Mll)
     X = X1[:,1:n]; V = X1'*Kll*X1; Z = 10e-6
     Kmm = X'*Kll*X; b = -X*inv(Kmm)*X'*Klr
     B = -X1*inv(V)*X1'*Klr; Kbb = Krr + Krl*B
