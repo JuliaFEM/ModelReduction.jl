57b6f90d84f7e822dbdc262aeb77373a3c6139ab
Marja Rapo <mra.rapo@gmail.com>
2018-05-02 17:11:57 +0300

    Update craig_bampton.jl

--- src/craig_bampton.jl
+++ src/craig_bampton.jl
@@ -10,21 +10,16 @@ r = retained DOF:s, l = internal DOF:s, n = the number of modes to keep.
 
 """
 function craig_bampton(K, M, r, l, n)
-    Krr = K[r,r]; println("Krr completed.")
-    Krl = K[r,l]; println("Krl completed.")
-    Klr = K[l,r]; println("Klr completed.")
-    Kll = K[l,l]; println("Kll completed.")
-    Mrr = M[r,r]; println("Mrr completed.")
-    Mrl = M[r,l]; println("Mrl completed.")
-    Mlr = M[l,r]; println("Mlr completed.")
-    Mll = M[l,l]; println("Mll completed.")
+    Krr = K[r,r]; Krl = K[r,l] 
+    Klr = K[l,r]; Kll = K[l,l] 
+    Mrr = M[r,r]; Mrl = M[r,l] 
+    Mlr = M[l,r]; Mll = M[l,l]
     if issparse(K) && issparse(M)
         w2, X1 = eigs(Kll, Mll)
     else
         w2 = eigvals(Kll,Mll)
         X1 = eigvecs(Kll,Mll)
     end
-    println("Eigenvalues and eigenvectors of Kll and Kmm calculated.")
     X = X1[:,1:n]; V = X1'*Kll*X1; Z = 10e-6
     Kmm = X'*Kll*X; b = -X*inv(Kmm)*X'*Klr
     B = -X1*inv(V)*X1'*Klr; Kbb = Krr + Krl*B
